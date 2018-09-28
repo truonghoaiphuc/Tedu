67f265feb6a59b57a5e9723e67679de86ef1499f
TRUONG HOAI PHUC <phucvsd@gmail.com>
2018-09-28 11:43:25 +0700

    bai 5

---
+++ TeduShop.Model/Abstratct/IAuditable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstratct
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string UpdatedBy { get; set; }
+
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+
+        bool Status { get; set; }
+    }
+}
