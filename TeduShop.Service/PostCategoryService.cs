119d05ec8c9ce1309777d18f4f1e09f5d00816a2
truonghoaiphuc <phucvsd@gmail.com>
2018-10-01 01:14:50 +0700

    30092018

---
+++ TeduShop.Service/PostCategoryService.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IPostCategoryService
+    {
+        PostCategory Add(PostCategory postcategory);
+
+        void Update(PostCategory postcategory);
+
+        PostCategory Delete(int id);
+
+        IEnumerable<PostCategory> GetAll();
+
+        IEnumerable<PostCategory> GetAllByParentID(int parentID);
+
+        PostCategory GetById(int id);
+
+        void SaveChanges();
+    }
+
+    public class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        IUnitOfWork _unitOfWork;
+
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public PostCategory Add(PostCategory postcategory)
+        {
+            return _postCategoryRepository.Add(postcategory);
+        }
+
+        public PostCategory Delete(int id)
+        {
+            return _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentID(int parentID)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentID);
+        }
+
+        public PostCategory GetById(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(PostCategory postcategory)
+        {
+            _postCategoryRepository.Update(postcategory);
+        }
+    }
+}
